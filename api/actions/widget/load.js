be6737edb63257e1ead747000a07a0c4c71679eb
Rob Halff <rob.halff@gmail.com>
2016-01-26 00:11:29 +0100

    do not fail

--- api/actions/widget/load.js
+++ api/actions/widget/load.js
@@ -18,11 +18,7 @@ export default function load(req) {
   return new Promise((resolve, reject) => {
     // make async call to database
     setTimeout(() => {
-      if (Math.random() < 0.33) {
-        reject('Widget load fails 33% of the time. You were unlucky.');
-      } else {
-        resolve(getWidgets(req));
-      }
+      resolve(getWidgets(req));
     }, 1000); // simulate async load
   });
 }
